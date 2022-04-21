123031599fbe1c3cedd3901aa14fc3f08d8ed63b
Abideen <Abideenali926@gmail.com>
2022-04-21 00:49:22 -0700

    costom range slider

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Rangeslider from './components/Rangeslider'
+
+function App() {
+  return (
+    <Rangeslider />
+  );
+}
+
+export default App;
+
+
+// netlify url https://custom-range-slider-44.netlify.app/
